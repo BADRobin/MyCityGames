3e018f53130e6eab32452125ad6081ac2451fbf9
BADrobin <olegbryl@gmail.com>
2023-07-16 18:52:47 +0200

    123

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,22 @@ public class Main {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         fillCities();
-//        String input = scanner. nextLine();
-//       System.out.println(cities);
-//        System.out.println("Input: " + input);
         gameLoop();
     }
     private static void gameLoop(){
         while (true){
             String input = scanner.nextLine();
             if (input.equals("exit")){
-                System.out.println("Дякую за гру");
+                System.out.println("Дякую за гру!");
                 System.exit(0);
             }
            char lastChar =  input.charAt(input.length() - 1);
-//            System.out.println("Lasr Char: " + lastChar);
             for (String city: cities){
                 char firstChar = city.charAt(0);
                 if (Character.toLowerCase(lastChar) == Character.toLowerCase(firstChar)){
                     System.out.println("Моє місто: " + city);
                     break;
                 }
-
             }
         }
     }
@@ -39,10 +34,6 @@ public class Main {
         cities.add("Алчевськ");
         cities.add("Житомир");
         cities.add("Львів");
-        cities.add("Луцьк");
-        cities.add("Рівно");
-
-
-
-    }
+        cities.add("Рівне");
+   }
 }
